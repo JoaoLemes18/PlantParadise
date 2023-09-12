9f81b9c75c69595879b174b5770ba9bb563f139a
JoaoLemes18 <joaolemes17@hotmail.com>
2023-09-12 01:09:15 -0400

    fix: componentizing the stick card style

--- src/components/Cards/VerticalCard/VerticalCard.tsx
+++ src/components/Cards/VerticalCard/VerticalCard.tsx
@@ -1,14 +1,8 @@
 import React, { useState } from "react";
-import {
-  View,
-  Text,
-  Image,
-  TouchableOpacity,
-  StyleSheet,
-  Platform,
-} from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { useFavorite } from "../../../context/Favorite";
+import { styles } from "./styles";
 
 const LongCardButton = ({
   imageSource,
@@ -75,89 +69,5 @@ const LongCardButton = ({
     </TouchableOpacity>
   );
 };
-
-const styles = StyleSheet.create({
-  card: {
-    backgroundColor: "#fff",
-    width: 300,
-    height: 280,
-    borderRadius: 8,
-    marginLeft: 10,
-    marginBottom: 10,
-    marginTop: 20,
-    overflow: "hidden",
-    position: "relative",
-  },
-  favoriteButton: {
-    position: "absolute",
-    top: 10,
-    left: 10,
-    backgroundColor: "transparent",
-    zIndex: 1,
-  },
-  image: {
-    width: "100%",
-    aspectRatio: 1.5,
-    resizeMode: "cover",
-  },
-  content: {
-    padding: 16,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: "bold",
-  },
-  value: {
-    fontSize: 16,
-    color: "#666",
-  },
-  cartButton: {
-    backgroundColor: "#418B64",
-    borderRadius: 50,
-    height: 30,
-    padding: 8,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  heartButton: {
-    position: "absolute",
-    top: 10,
-    left: 10,
-    backgroundColor: "white",
-    borderRadius: 50,
-    width: 35,
-    height: 35,
-    alignItems: "center",
-    justifyContent: "center",
-    ...Platform.select({
-      ios: {
-        shadowColor: "black",
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-      },
-      android: {
-        elevation: 3,
-      },
-    }),
-  },
-  heartButtonClicked: {
-    backgroundColor: "white",
-    ...Platform.select({
-      ios: {
-        shadowColor: "black",
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-      },
-      android: {
-        elevation: 3,
-      },
-    }),
-  },
-});
 
 export default LongCardButton;
