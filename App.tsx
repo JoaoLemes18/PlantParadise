ccecab39f83164dd7032714e9403797019ececc8
JoaoLemes18 <joaolemes17@hotmail.com>
2023-09-13 18:38:25 -0400

    finished project

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { StatusBar } from "react-native";
-import { useFonts } from "expo-font";
 import { TabRoutes } from "./src/routes/tabs.routes";
 import CartProvider from "./src/context/Cart";
 import FavoriteProvider from "./src/context/Favorite";
